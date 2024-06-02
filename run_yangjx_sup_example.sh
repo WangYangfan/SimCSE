d1e9dc5defd2221b8df1adf847e928bcd144102f
Wayfarer <32709746+WangYangfan@users.noreply.github.com>
2024-06-02 23:33:38 +0800

    Create run_yangjx_sup_example.sh

---
+++ run_yangjx_sup_example.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES=3 python train.py  \
+    --train_dataset ../princeton_data/nli   \
+    --batch_size 64 \
+    --lr 3e-5   \
+    --num_shards 100    \
+    --save_info yangjx_sup
